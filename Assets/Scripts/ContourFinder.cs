b9c664bd35b54d4295324e0e6bebe813e33bad8a
david-3lm <d.3lopm@gmail.com>
2024-02-12 13:20:09 +0100

    Centers Cleaned

--- Assets/Scripts/ContourFinder.cs
+++ Assets/Scripts/ContourFinder.cs
@@ -17,8 +17,6 @@ public class ContourFinder : WebCam
     private Mat img;
     private Mat processedImg = new Mat();
     private Point[][] contours;
-    private List<Point> centers;
-    private List<Point> centersAux;
     private HierarchyIndex[] hierarchy;
     private Vector2[] vectorList;
 
@@ -44,10 +42,6 @@ public class ContourFinder : WebCam
         base.Awake();
         cam = Camera.main;
 
-        centers = new List<Point>();
-        centersAux = new List<Point>();
-
-
         //Adjust the limits
         minX =Limits.Instance.valuesX.Min();
         minY =Limits.Instance.valuesY.Min();
@@ -60,7 +54,7 @@ public class ContourFinder : WebCam
     private void Update()
     {
         base.Update();
-        ProcessTexture();       
+        ProcessTexture();
     }
     private void OnApplicationQuit()
     {
@@ -72,35 +66,15 @@ public class ContourFinder : WebCam
     private void ThreadMethod()
     {
         while (t.IsAlive)
-            ValidateCenters();
+            ValidateThread();
     }
     /// <summary>
     /// Gets the a list of detected centers and validates them inside the limits
     /// </summary>
-    public void ValidateCenters()
+    public void ValidateThread()
     {
         if (contours == null || imgHilo) return;
-        Mat mat = processedImg;
 
-        if (centers.Count < 1) return;
-        //Validate the centers
-        List<Point> centerAux = new List<Point>();
-        centerAux.AddRange(centers);
-        List<Point> validatedCenters = new List<Point>();
-        foreach (Point p in centerAux)
-        {
-            bool isInside = true;
-            if (p.X <= minX || p.X >= maxX || p.Y <= minY || p.Y >= maxY)
-            {
-                isInside = false;
-            }
-            if(isInside)
-            {
-                validatedCenters.Add(p);
-            }
-        }
-
-        DrawCenters(mat, new Scalar(127, 127, 127), 20, validatedCenters);
         imgHilo = true;
     }
 
@@ -147,9 +121,9 @@ public class ContourFinder : WebCam
             Cv2.Threshold(processedImg, processedImg, threshold, 255, ThresholdTypes.BinaryInv);
             Cv2.FindContours(processedImg, out contours, out hierarchy, RetrievalModes.Tree, ContourApproximationModes.ApproxSimple, null);
 
-            ComputeCenter(processedImg, contours, out centers);
 
             SetImgProcessed(processedImg);
+
         }
     }
     /// <summary>
@@ -158,48 +132,18 @@ public class ContourFinder : WebCam
     /// <param name="processedImg"></param>
     /// <param name="contours">Contours of the objects detected by OpenCV</param>
     /// <param name="centersList">The list of centers</param>
-    private void ComputeCenter(Mat processedImg, Point[][] contours, out List<Point> centersList)
-    {
-        Moments m;
-        centersList = centers;
-        centersList.Clear();
-        foreach (var cont in contours)
-        {
-            double area= Cv2.ContourArea(cont);
-            if (area > minArea)
-            {
-                m = Cv2.Moments(cont);
-                Point c = new(m.M10 / m.M00, m.M01 / m.M00);
-                if (!centersAux.Contains(c))
-                {
-                    centersList.Add(c);
-                }
-            }
-        }
-        
-    }
 
     private Vector2[] PointsToVector2(Point[] points)
     {
         vectorList = new Vector2[points.Length];
         for (int i = 0; i < points.Length; i++)
         {
-            
             vectorList[i] = new Vector2(points[i].X, points[i].Y);
             vectorList[i]=cam.ScreenToWorldPoint(vectorList[i]);
         }
         return vectorList;
     }
-    private void DrawCenters(Mat image, Scalar color, int thickness, List<Point> centers)
-    {
-        for (int i = 1; i < centers.Count; i++)
-        {
-            Cv2.Circle(image, centers[i],6,color,thickness);
-        }
-        centersAux.Clear();
-        centersAux = centers;
-        centers.Clear();
-    }
+
     /// <summary>
     /// Function that gets the color at a certain coord in the camera. If black there is an object.
     /// </summary>
